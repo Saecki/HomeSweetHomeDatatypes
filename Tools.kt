083a177f24f12f2cfcd4a57698c2089c887a56f1
Saecki <tobiasschmitz2001@gmail.com>
2020-05-03 15:48:05 +0200

    implemented update method for mutable lists

--- Tools.kt
+++ Tools.kt
@@ -13,6 +13,16 @@ inline fun <T> MutableList<T>.upsert(element: T, predicate: (T) -> Boolean): Boo
     }
 }
 
+inline fun <T> MutableList<T>.update(element: T, predicate: (T) -> Boolean): Boolean {
+    val index = this.indexOfFirst(predicate)
+    return if (index == -1) {
+        false
+    } else {
+        this[index] = element
+        true
+    }
+}
+
 fun <T : Comparable<T>> T.clamped(min: T, max: T) = when {
     this < min -> min
     this > max -> max
