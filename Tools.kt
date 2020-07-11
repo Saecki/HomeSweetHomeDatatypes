13c49b54309d0dba5d2e6f1ffb919d69b01e6a65
Saecki <tobiasschmitz2001@gmail.com>
2020-07-11 17:31:17 +0200

    update

--- Tools.kt
+++ Tools.kt
@@ -2,7 +2,28 @@ package bedbrains.shared.datatypes
 
 import java.io.*
 
-inline fun <T> List<T>.upsert(element: T, predicate: (T) -> Boolean): List<T> {
+inline fun <T> MutableList<T>.upsert(element: T, predicate: (T) -> Boolean): Boolean {
+    val index = this.indexOfFirst(predicate)
+    return if (index == -1) {
+        this.add(element)
+        false
+    } else {
+        this[index] = element
+        true
+    }
+}
+
+inline fun <T> MutableList<T>.update(element: T, predicate: (T) -> Boolean): Boolean {
+    val index = this.indexOfFirst(predicate)
+    return if (index == -1) {
+        false
+    } else {
+        this[index] = element
+        true
+    }
+}
+
+inline fun <T> List<T>.upserted(element: T, predicate: (T) -> Boolean): List<T> {
     val index = this.indexOfFirst(predicate)
     return if (index == -1) {
         this.plus(element)
@@ -11,7 +32,7 @@ inline fun <T> List<T>.upsert(element: T, predicate: (T) -> Boolean): List<T> {
     }
 }
 
-inline fun <T> List<T>.update(element: T, predicate: (T) -> Boolean): List<T> {
+inline fun <T> List<T>.updated(element: T, predicate: (T) -> Boolean): List<T> {
     return this.map { if (predicate(it)) element else it }
 }
 
